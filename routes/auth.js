304e1f8b2aca65cc7326c30f145009e6e15bf99d
Ruize Nie <rz376775@dal.ca>
2020-08-03 23:40:15 -0300

    joi added to validate the data from user input

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const { registerValidation } = require("../validation");
 
-router.post("/signup", async (req, res) => {
+router.post("/register", async (req, res) => {
+  // Validate date before register user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
